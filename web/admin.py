80b25537cc6def12974d6f8024700cd3d751646e
Nyongesawakos <108975266+Nyongesawakos@users.noreply.github.com>
2025-06-12 11:41:19 +0300

    Add files via upload

---
+++ web/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import user, tips,room, message,topic, update,cash_expenditure
+admin.site.register(user)
+admin.site.register(tips)
+admin.site.register(room)
+admin.site.register(message)
+admin.site.register(topic)
+admin.site.register(update)
+admin.site.register(cash_expenditure)
+
+
+
+
